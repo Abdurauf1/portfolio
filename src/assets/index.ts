1dea5a2d976445bfc2357ccdbc59c54fcfd1d7cb
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-07-21 03:51:19 +0500

    update

---
+++ src/assets/index.ts
@@ -0,0 +1,45 @@
+import backend from "./images/backend.png";
+import creator from "./images/creator.png";
+import mobile from "./images/mobile.png";
+import web from "./images/web.png";
+import github from "./images/github.png";
+import menu from "./icons/menu.svg";
+import close from "./icons/close.svg";
+
+import bootstrap from "./images/skills/bootstrap.png"
+import css from "./images/skills/css.png";
+import git from "./images/skills/git.png";
+import html from "./images/skills/html.png";
+import javascript from "./images/skills/javascript.png";
+import mysql from "./images/skills/mysql.png";
+import nodejs from "./images/skills/nodejs.png";
+import npm from "./images/skills/npm.png";
+import reactjs from "./images/skills/reactjs.png";
+import sass from "./images/skills/sass.png";
+import tailwind from "./images/skills/tailwind.png";
+import typescript from "./images/skills/typescript.png";
+
+import itransition from "./images/company/itransition.svg";
+
+export {
+  mysql,
+  sass,
+  itransition,
+  npm,
+  bootstrap,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+};
---
+++ src/constants/index.ts
@@ -0,0 +1,39 @@
+import {
+    mysql,
+    sass,
+    itransition,
+    npm,
+    bootstrap,
+    backend,
+    creator,
+    mobile,
+    web,
+    github,
+    menu,
+    close,
+    css,
+    git,
+    html,
+    javascript,
+    nodejs,
+    reactjs,
+    tailwind,
+    typescript,
+} from "../assets"
+
+const technologies = [
+    {
+        name: "HTML",
+        icon: html
+    },
+    {
+        name: "CSS",
+        icon: css
+    },
+    {
+        name: "Scss",
+        icon: sass
+    },
+]
+
+export { technologies }
