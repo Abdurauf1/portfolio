a9acff310d7b912a47cc5686e082541cf07be9b5
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-07-28 02:16:32 +0500

    fix: unused STATE

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -8,7 +8,7 @@ const Stars = (props: any) => {
 
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
-  useFrame((state, delta: any) => {
+  useFrame((_state, delta: any) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
