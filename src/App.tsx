36945f45416509008aadd5ff17f9c4f4e68a7262
Abdurauf <aabdurauf004@gmail.com>
2023-12-31 21:41:33 +0500

    bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+        <div className="w-full bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
           <Hero />
         </div>
