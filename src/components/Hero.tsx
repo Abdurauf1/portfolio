55e3529dca4f98541728117540e127905c5f2bed
Abdurauf <aabdurauf004@gmail.com>
2024-06-03 20:08:16 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} justify-between absolute inset-0 top-[120px] max-w-7xl mx-auto flex lg:flex-row items-start flex-col-reverse gap-5`}
+        className={`${styles.paddingX} justify-between absolute inset-0 top-[120px] max-w-7xl mx-auto flex lg:flex-row items-start flex-col gap-5`}
       >
         <div className="flex flex-row items-start gap-5">
           <div className="flex flex-col justify-center items-center mt-5">
