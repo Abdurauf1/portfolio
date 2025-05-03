32f254d1867735efdd440f36270cceee2371247a
Abdurauf <aabdurauf004@gmail.com>
2025-05-03 21:56:13 +0500

    change projects card

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -34,24 +34,19 @@ const ProjectCard = ({
         className="bg-tertiary p-5 rounded-2xl w-full sm:w-[360px]"
       >
         <div className="relative w-full h-[230px]">
-          <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
-              <img src={github2} alt="github" className="w-1/2 h-1/2 object-contain" />
-            </div>
+          <a href={deploy} target="_blank">
+            <img src={image} alt={name} className="w-full h-full object-cover rounded-2xl" />
+          </a>
+          <div
+            onClick={() => window.open(source_code_link, "_blank")}
+            className="absolute top-3 right-3 black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+          >
+            <img src={github2} alt="github" className="w-1/2 h-1/2 object-contain" />
           </div>
         </div>
 
         <div className="mt-5">
-          <div className="flex items-center justify-between">
-            <h3 className="text-white font-bold text-[24px]">{name}</h3>
-            <a href={deploy} target="blank" className="hover:text-secondary">
-              Live
-            </a>
-          </div>
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
           <p className="mt-2 text-secondary text-[14px]">{description}</p>
         </div>
 
