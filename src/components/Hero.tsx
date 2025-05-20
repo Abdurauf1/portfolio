2e4a826ff3ea23db8faaa77a22339682349c29b3
Abdurauf <aabdurauf004@gmail.com>
2025-05-21 01:07:32 +0500

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -31,10 +31,6 @@ const Hero = () => {
           variants={fadeIn("left", "spring", 0.2, 1)}
           className="w-full lg:w-1/2 bg-gradient-to-r from-blue-750 to-indigo-900 rounded-lg select-none border border-blue-850 relative"
         >
-          <div className="flex flex-row">
-            <div className="h-[1px] bg-gradient-to-r from-transparent via-pink-500 to-violet-600 w-full"></div>
-            <div className="h-[1px] bg-gradient-to-r from-violet-600 to-transparent w-full"></div>
-          </div>
           <div className="px-8 py-5">
             <div className="flex flex-row space-x-2">
               <div className="cursor-pointer rounded-full w-3 h-3 bg-red-400"></div>
@@ -42,7 +38,7 @@ const Hero = () => {
               <div className="cursor-pointer rounded-full w-3 h-3 bg-green-200"></div>
             </div>
           </div>
-          <div className="px-8 py-8 border-t-[2px] border-indigo-900 overflow-hidden">
+          <div className="px-8 py-8 border-t-[2px] overflow-hidden">
             <code>
               <div className="blink">
                 <span className="font-mono text-pink-500 mr-2">const</span>
