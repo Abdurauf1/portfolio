387968f5bbc8ffc70cdf0ca5f36d40604ec50d35
Abdurauf <aabdurauf004@gmail.com>
2025-06-22 20:35:13 +0500

    update

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { MutatingDots } from "react-loader-spinner"
 
 const Loading = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
+    <div className="dark:bg-primary bg-primaryLight h-screen flex items-center justify-center">
       <MutatingDots
         color="#ec4899"
         secondaryColor="#ec4899"
